47da21ea6cea17c6d071e0fd5c4e481187ea9451
HelenDeunerFerreira <helendeunerferreira@gmail.com>
2022-10-13 16:42:01 -0300

    Ainda trabalhando na solução do K weakest rows in a matrix

--- k-weakest.py
+++ k-weakest.py
@@ -1,14 +1,23 @@
 class Solution:
-    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-
-        ones = 1
+    def kWeakestRows(self, mat, k):
         rows = []
 
         for i in mat:
+            ones = 0
             for j in i:
                 if j == 1:
                     ones += 1
             rows.append(ones)
 
-        for soldiers in rows:
-            pass
+        weak = sorted(rows)
+
+        print(weak[0:k])
+
+
+# A row i is weaker than a row j if one of the following is true:
+# The number of soldiers in row i is less than the number of soldiers in row j.
+# Both rows have the same number of soldiers and i < j.
+# Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+
+s = Solution()
+s.kWeakestRows([[1, 0, 0, 0], [1, 1, 1, 1], [1, 0, 0, 0], [1, 0, 0, 0]], 2)
